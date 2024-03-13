f0662134c64d303eb8a68976bb7b5bc969e0ad6c
Nélida Valero <nelidavalero06@gmail.com>
2024-03-12 20:33:39 -0500

    🚧 WIP config terraform

--- main.tf
+++ main.tf
@@ -17,6 +17,8 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  key_name      = "key"
+  security_groups = [aws_security_group.app_server.name]
 
   user_data = <<-EOF
   #!/bin/bash
@@ -27,5 +29,49 @@ resource "aws_instance" "app_server" {
 
   tags = {
     Name = "challenge-email"
+  }
+}
+
+resource "aws_security_group" "app_server" {
+  name        = "app_server_challenge_email"
+  description = "Security group for the application server"
+
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["190.24.77.141/32"]  # Permitir solo tu dirección IP para SSH
+  }
+
+  ingress {
+    from_port   = 4080
+    to_port     = 4080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5173
+    to_port     = 5173
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app_server_challenge_email"
   }
 }
